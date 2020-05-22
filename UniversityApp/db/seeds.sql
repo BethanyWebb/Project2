08dd7a90c2717e1d2f2effd95261a6f455a444ba
Aref Smiley <aref.smiley@yahoo.com>
2020-05-21 21:52:39 -0400

    workinggit add -A!

--- UniversityApp/db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE university_db;
-
-INSERT INTO university (first_name, last_name, email_address, nationality, highest_education, desired_country, student_visa)
-VALUES ("Jane", "Doe","missing@gmail.com","Swedish","Bachelors","Canada", true);
-
--- appidea.md
+++
@@ -1,9 +0,0 @@
-Matches applicants with college based on: 
-
-Major
-degree length
-tuition prices
-programs offered
-
-using apis: Matchcollege.com for college information
-            Yandex to translate 
